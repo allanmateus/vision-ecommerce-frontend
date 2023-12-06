b9f2a82b66db74f9ecebfc5f3eed0aa559918ef7
allanmateus <allanpmateus@gmail.com>
2023-12-06 16:57:12 -0300

    Criação de componente de catalogo e rotaDesconh.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 import Inicial from './pages/inicial/Inicial';
 import Header from './components/header/Header';
 import Cadastro from './pages/cadastro/Cadastro';
+import Catalogo from './pages/catalogo/Catalogo';
 import { Route, Routes } from "react-router-dom";
 import React from "react";
+import RotaDesconhecida from './pages/erro/RotaDesconhecida';
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <Routes>
-          <Route path="/" element={<Inicial/>}  /> 
-          <Route path="/cadastro" element={<Cadastro/>}  />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Inicial/>}  /> 
+        <Route path="/cadastro" element={<Cadastro/>}  />
+        <Route path="/catalogo" element={<Catalogo/>}  />
+        <Route path="*" element={<RotaDesconhecida/>}  />
+      </Routes>
     </div>
   );
 }
--- src/pages/inicial/Inicial.jsx
+++ src/pages/inicial/Inicial.jsx
@@ -3,15 +3,16 @@ import React from "react";
 function Inicial() {
   return (
     <div className="container">
-      <div className="row d-flex flex-column align-items-center">
-        <div className="col ">
+      <div className="d-flex">
+        <div className="mt-auto">
           <h3>Olá, colaborador(a)!</h3>
           <h3>Seja muito bem vindo(a).</h3>
           <h4>Vamos juntos oferecer os melhores produtos pelos menores preços.<br/> A excelência no atendimento é o nosso padrão.</h4>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Inicial;
+export default Inicial
+
